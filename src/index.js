2befa9b09eb5db55c06587575b55532b400039d9
Cesar Denis <cesar@cesardenis.com>
2019-04-17 19:16:34 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// eslint-disable-next-line import/no-unresolved
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
